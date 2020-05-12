30184ce67a718ca7d11887ccefb632323b25a03e
Ahmer Afzal <ahmerafzal@yahoo.com>
2020-05-12 11:08:28 +0500

    More options added

--- Youtube-DL.bat
+++ Youtube-DL.bat
@@ -8,44 +8,75 @@ TITLE "Download HD Videos Using Youtube-DL | Ahmer Afzal"
 SET DOWNLOADLOCATION=".//Download//"
 SET YOUTUBEDLLOCATION="Files//youtube-dl.exe"
 SET FFMPEGLOCATION="Files//"
-SET BATCHFILE="Batch-Downloads.txt"
-SET MERGEOUTPUTFORMAT="mkv
-SET AUDIOARGS="(bestaudio[acodec=opus]/bestaudio[acodec=vorbis]/bestaudio[acodec=aac]/bestaudio[acodec=m4a]/bestaudio)/best"
-SET VIDEOARGS="(bestvideo[vcodec^=av01][height>=3072][fps>30]/bestvideo[vcodec=vp9.2][height>=3072][fps>30]/bestvideo[vcodec=vp9][height>=3072][fps>30]/bestvideo[vcodec^=av01][height>=3072]/bestvideo[vcodec=vp9.2][height>=3072]/bestvideo[vcodec=vp9][height>=3072]/bestvideo[height>=3072]/bestvideo[vcodec^=av01][height>=2160][fps>30]/bestvideo[vcodec=vp9.2][height>=2160][fps>30]/bestvideo[vcodec=vp9][height>=2160][fps>30]/bestvideo[vcodec^=av01][height>=2160]/bestvideo[vcodec=vp9.2][height>=2160]/bestvideo[vcodec=vp9][height>=2160]/bestvideo[height>=2160]/bestvideo[vcodec^=av01][height>=1440][fps>30]/bestvideo[vcodec=vp9.2][height>=1440][fps>30]/bestvideo[vcodec=vp9][height>=1440][fps>30]/bestvideo[vcodec^=av01][height>=1440]/bestvideo[vcodec=vp9.2][height>=1440]/bestvideo[vcodec=vp9][height>=1440]/bestvideo[height>=1440]/bestvideo[vcodec^=av01][height>=1080][fps>30]/bestvideo[vcodec=vp9.2][height>=1080][fps>30]/bestvideo[vcodec=vp9][height>=1080][fps>30]/bestvideo[vcodec^=av01][height>=1080]/bestvideo[vcodec=vp9.2][height>=1080]/bestvideo[vcodec=vp9][height>=1080]/bestvideo[height>=1080]/bestvideo[vcodec^=av01][height>=720][fps>30]/bestvideo[vcodec=vp9.2][height>=720][fps>30]/bestvideo[vcodec=vp9][height>=720][fps>30]/bestvideo[vcodec^=av01][height>=720]/bestvideo[vcodec=vp9.2][height>=720]/bestvideo[vcodec=vp9][height>=720]/bestvideo[height>=720]/bestvideo)"
+SET BATCHFILE="URLs.txt"
+SET ARCHIVE="Downloaded.log"
+:: SET AUDIOARGS="(bestaudio[acodec=opus]/bestaudio[acodec=vorbis]/bestaudio[acodec=aac]/bestaudio[acodec=m4a]/bestaudio)/best"
+:: SET VIDEOARGS="(bestvideo[height>=1080][fps>30]/bestvideo[vcodec=vp9.2][height>=1080][fps>30]/bestvideo[vcodec=vp9][height>=1080][fps>30]/bestvideo[vcodec^=av01][height>=1080]/bestvideo[vcodec=vp9.2][height>=1080]/bestvideo[vcodec=vp9][height>=1080]/bestvideo[height>=1080]/bestvideo[vcodec^=av01][height>=720][fps>30]/bestvideo[vcodec=vp9.2][height>=720][fps>30]/bestvideo[vcodec=vp9][height>=720][fps>30]/bestvideo[vcodec^=av01][height>=720]/bestvideo[vcodec=vp9.2][height>=720]/bestvideo[vcodec=vp9][height>=720]/bestvideo[height>=720]/bestvideo)"
+
+SET VIDMP4="bestvideo[height<=4320][fps<60][ext=mp4]+bestaudio[ext=m4a]/bestvideo[height<=2160][fps<60][ext=mp4]+bestaudio[ext=m4a]/bestvideo[height<=1440][fps<60][ext=mp4]+bestaudio[ext=m4a]/bestvideo[height<=1080][fps<60][ext=mp4]+bestaudio[ext=m4a]/bestvideo[height<=720][fps<60][ext=mp4]+bestaudio[ext=m4a]/mp4"
+
+SET NEWARG="bestvideo[ext=webm][height>=4320][fps=60]+bestaudio/bestvideo[ext=mp4][height>=4320][fps=60]+bestaudio/bestvideo[ext=webm][height>=4320]+bestaudio/bestvideo[ext=mp4]+bestaudio/bestvideo+bestaudio/bestvideo[ext=webm][height>=2160][fps=60]+bestaudio/bestvideo[ext=mp4][height>=2160][fps=60]+bestaudio/bestvideo[ext=webm][height>=2160]+bestaudio/bestvideo[ext=mp4]+bestaudio/bestvideo+bestaudio/bestvideo[ext=webm][height>=1080][fps=60]+bestaudio/bestvideo[ext=mp4][height>=1080][fps=60]+bestaudio/bestvideo[ext=webm][height>=1080]+bestaudio/bestvideo[ext=mp4]+bestaudio/bestvideo+bestaudio/bestvideo[ext=webm][height>=720][fps=60]+bestaudio/bestvideo[ext=mp4][height>=720][fps=60]+bestaudio/bestvideo[ext=webm][height>=720]+bestaudio/bestvideo[ext=mp4]+bestaudio/bestvideo+bestaudio/best"
+
+SET VIDMP4AHMER="bestvideo[height<=720][fps<60][ext=mp4]+bestaudio[ext=m4a]/mp4"
 
 :menu
 ECHO.
-ECHO [1] Download Videos
-ECHO [2] Download Audios
-ECHO [3] Update Youtube-DL
-ECHO [4] EXIT
+ECHO [1] Download Videos Full HD (Convert into MKV)
+ECHO.
+ECHO [2] Download Videos MP4 (May not full HD)
+ECHO.
+ECHO [3] Download Audios (MP3)
+ECHO.
+ECHO [4] 720P For Ahmer Afzal
+ECHO.
+ECHO [5] Update Youtube-DL
+ECHO.
+ECHO [6] EXIT
+
 ECHO.
 SET CHOICE=0
 SET /P CHOICE="Please choose one option: 
 ECHO.
-IF "%CHOICE%"=="1" GOTO video
-IF "%CHOICE%"=="2" GOTO audio
-IF "%CHOICE%"=="3" GOTO update
-IF "%CHOICE%"=="4" GOTO :EOF
+IF "%CHOICE%"=="1" GOTO MKV
+IF "%CHOICE%"=="2" GOTO MP4
+IF "%CHOICE%"=="3" GOTO MP3
+IF "%CHOICE%"=="4" GOTO AHMER
+IF "%CHOICE%"=="5" GOTO UPDATE
+IF "%CHOICE%"=="6" GOTO :EOF
 ECHO.
 ECHO Invalid option
 TIMEOUT /T 2 /NOBREAK > NUL
 CLS
 GOTO menu
 
-:video
-%YOUTUBEDLLOCATION% --no-check-certificate --ignore-errors --ignore-config --continue --newline --no-part --add-metadata --all-subs --embed-subs --format %VIDEOARGS%+%AUDIOARGS% --output "%DOWNLOADLOCATION%%%(title)s.%%(ext)s" --batch-file %BATCHFILE% --download-archive "Downloaded.log" --ffmpeg-location %FFMPEGLOCATION% --merge-output-format %MERGEOUTPUTFORMAT%
+:MKV
+%YOUTUBEDLLOCATION% --no-check-certificate --ignore-errors --ignore-config --continue --yes-playlist --prefer-ffmpeg --no-part --add-metadata --format %NEWARG% --output "%DOWNLOADLOCATION%%%(id)s" --batch-file %BATCHFILE% --download-archive %ARCHIVE% --merge-output-format "mkv" --ffmpeg-location %FFMPEGLOCATION%
+ECHO.
+TIMEOUT /T 6 /NOBREAK > NUL
+CLS
+GOTO menu
+
+:MP4
+%YOUTUBEDLLOCATION% --no-check-certificate --ignore-errors --ignore-config --continue --yes-playlist --prefer-ffmpeg --no-part --add-metadata --format %VIDMP4% --output "%DOWNLOADLOCATION%%%(title)s.%%(ext)s" --batch-file %BATCHFILE% --download-archive %ARCHIVE% --merge-output-format "mp4" --ffmpeg-location %FFMPEGLOCATION%
+ECHO.
+TIMEOUT /T 6 /NOBREAK > NUL
+CLS
+GOTO menu
+
+:MP3
+%YOUTUBEDLLOCATION% --no-check-certificate --ignore-errors --ignore-config --continue --yes-playlist --no-part --add-metadata --all-subs --embed-thumbnail --embed-subs -x --audio-format "mp3" --audio-quality 0 --output "%DOWNLOADLOCATION%%%(title)s.%%(ext)s" --batch-file %BATCHFILE% --download-archive %ARCHIVE% --ffmpeg-location %FFMPEGLOCATION%
 ECHO.
 CLS
 GOTO menu
 
-:audio
-%YOUTUBEDLLOCATION% --no-check-certificate --ignore-errors --ignore-config --continue --newline --no-part --add-metadata --all-subs --embed-thumbnail --embed-subs -x --audio-format "mp3" --audio-quality 0 --output "%DOWNLOADLOCATION%%%(title)s.%%(ext)s" --batch-file %BATCHFILE% --download-archive "Downloaded.log" --ffmpeg-location %FFMPEGLOCATION%
+:AHMER
+%YOUTUBEDLLOCATION% --no-check-certificate --ignore-errors --ignore-config --continue --yes-playlist --prefer-ffmpeg --no-part --add-metadata --format %VIDMP4AHMER% --output "%DOWNLOADLOCATION%%%(title)s.%%(ext)s" --batch-file %BATCHFILE% --download-archive %ARCHIVE% --merge-output-format "mp4" --ffmpeg-location %FFMPEGLOCATION%
 ECHO.
+TIMEOUT /T 6 /NOBREAK > NUL
 CLS
 GOTO menu
 
-:update
+:UPDATE
 ECHO Current version is 
 %YOUTUBEDLLOCATION% --version
 %YOUTUBEDLLOCATION% --update
