e769e8e1f41a7c15a1fce5914a5397244fa4de2e
Ahmer Afzal <ahmerafzal@yahoo.com>
2020-05-14 13:23:36 +0500

    Fix fps frame rate

--- Youtube-DL.bat
+++ Youtube-DL.bat
@@ -10,24 +10,22 @@ SET YOUTUBEDLLOCATION="Files//youtube-dl.exe"
 SET FFMPEGLOCATION="Files//"
 SET BATCHFILE="URLs.txt"
 SET ARCHIVE="Downloaded.log"
-:: SET AUDIOARGS="(bestaudio[acodec=opus]/bestaudio[acodec=vorbis]/bestaudio[acodec=aac]/bestaudio[acodec=m4a]/bestaudio)/best"
-:: SET VIDEOARGS="(bestvideo[height>=1080][fps>30]/bestvideo[vcodec=vp9.2][height>=1080][fps>30]/bestvideo[vcodec=vp9][height>=1080][fps>30]/bestvideo[vcodec^=av01][height>=1080]/bestvideo[vcodec=vp9.2][height>=1080]/bestvideo[vcodec=vp9][height>=1080]/bestvideo[height>=1080]/bestvideo[vcodec^=av01][height>=720][fps>30]/bestvideo[vcodec=vp9.2][height>=720][fps>30]/bestvideo[vcodec=vp9][height>=720][fps>30]/bestvideo[vcodec^=av01][height>=720]/bestvideo[vcodec=vp9.2][height>=720]/bestvideo[vcodec=vp9][height>=720]/bestvideo[height>=720]/bestvideo)"
 
-SET VIDMP4="bestvideo[height<=4320][fps<60][ext=mp4]+bestaudio[ext=m4a]/bestvideo[height<=2160][fps<60][ext=mp4]+bestaudio[ext=m4a]/bestvideo[height<=1440][fps<60][ext=mp4]+bestaudio[ext=m4a]/bestvideo[height<=1080][fps<60][ext=mp4]+bestaudio[ext=m4a]/bestvideo[height<=720][fps<60][ext=mp4]+bestaudio[ext=m4a]/mp4"
+SET VIDMP4="bestvideo[height<=4320][fps<=60][ext=mp4]+bestaudio[ext=m4a]/bestvideo[height<=2160][fps<=60][ext=mp4]+bestaudio[ext=m4a]/bestvideo[height<=1440][fps<=60][ext=mp4]+bestaudio[ext=m4a]/bestvideo[height<=1080][fps<=60][ext=mp4]+bestaudio[ext=m4a]/bestvideo[height<=720][fps<=60][ext=mp4]+bestaudio[ext=m4a]/mp4"
 
 SET NEWARG="bestvideo[ext=webm][height>=4320][fps=60]+bestaudio/bestvideo[ext=mp4][height>=4320][fps=60]+bestaudio/bestvideo[ext=webm][height>=4320]+bestaudio/bestvideo[ext=mp4]+bestaudio/bestvideo+bestaudio/bestvideo[ext=webm][height>=2160][fps=60]+bestaudio/bestvideo[ext=mp4][height>=2160][fps=60]+bestaudio/bestvideo[ext=webm][height>=2160]+bestaudio/bestvideo[ext=mp4]+bestaudio/bestvideo+bestaudio/bestvideo[ext=webm][height>=1080][fps=60]+bestaudio/bestvideo[ext=mp4][height>=1080][fps=60]+bestaudio/bestvideo[ext=webm][height>=1080]+bestaudio/bestvideo[ext=mp4]+bestaudio/bestvideo+bestaudio/bestvideo[ext=webm][height>=720][fps=60]+bestaudio/bestvideo[ext=mp4][height>=720][fps=60]+bestaudio/bestvideo[ext=webm][height>=720]+bestaudio/bestvideo[ext=mp4]+bestaudio/bestvideo+bestaudio/best"
 
-SET VIDMP4AHMER="bestvideo[height<=720][fps<60][ext=mp4]+bestaudio[ext=m4a]/mp4"
+SET VIDMP4AHMER="bestvideo[height<=720][fps<=60][ext=mp4]+bestaudio[ext=m4a]/mp4"
 
 :menu
 ECHO.
-ECHO [1] Download Videos MP4 (99%% full HD)
+ECHO [1] Download HD Videos in MP4
 ECHO.
-ECHO [2] Download Videos Full HD (File convert into MKV)
+ECHO [2] Download Full HD Videos in MKV
 ECHO.
 ECHO [3] Download Audios (MP3)
 ECHO.
-ECHO [4] 720P For Ahmer Afzal
+ECHO [4] 720P in MP4 For Ahmer Afzal
 ECHO.
 ECHO [5] Update Youtube-DL
 ECHO.
